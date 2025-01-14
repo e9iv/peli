0745d5ccb821f5079f772678f47a81bcd50da034
vike <rotkiv.imeinivik@gmail.com>
2025-01-14 22:21:28 +0200

    zoom out mechanic
    +Improved intro functionality with changing scenes
    +You can now zoom out with camera(forgot to mention this on the earlier commit.

--- common/main/main.gd
+++ common/main/main.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
+@onready var video: VideoStreamPlayer = $intro/CanvasLayer/VideoStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var tween = get_tree().create_tween()
+	get_tree().paused = true
+	video.play()
+	video.finished.connect(self._on_video_finished)
 	tween.tween_property(self, "modulate", Color.WHITE, 2.5)
 	tween.tween_property(self, "modulate:a", 1, 5)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	
+func _on_video_finished():
+	get_tree().paused = false
